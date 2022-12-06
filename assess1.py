c98d7bc6b3237b30becae257e33807daf82b01ca
BerillVarga <berill.varga@gmail.com>
2022-12-06 10:49:52 +0100

    fetching data from db

--- assess1.py
+++ assess1.py
@@ -2,10 +2,23 @@
 import psycopg2
 
 # Creating connection
-conn = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="assessmentdb",
-        user="postgres",
-        password="****"
-        )
+def connection():
+    conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="assessmentdb",
+            user="postgres",
+            password = '***'
+            )
+    return conn
+
+
+def read_contact_list():
+    conn = connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM contact_list;")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
