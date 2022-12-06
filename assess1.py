93b967466cd0902f8282dfd30c6a84301b227bd4
BerillVarga <berill.varga@gmail.com>
2022-12-06 10:34:01 +0100

    creating connection

--- assess1.py
+++ assess1.py
@@ -1,2 +1,11 @@
 # Import module
 import psycopg2
+
+# Creating connection
+conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="assessmentdb",
+        user="postgres",
+        password="****"
+        )
